7d21e90101b9a936773e57826e3ee28f014e5e77
Fumio SAGAWA <fumio.sagawa@gmail.com>
2016-04-18 18:32:34 +0900

    Watchタスク追加

--- gulp_tasks/tasks/ejs.js
+++ gulp_tasks/tasks/ejs.js
@@ -2,11 +2,15 @@ const gulp = require('gulp');
 const ejs = require('gulp-ejs');
 const gutil = require('gulp-util');
 const htmlmin = require('gulp-htmlmin');
+const watch = require('gulp-watch');
 
-
-gulp.task('ejs', () => {
+gulp.task('ejs-build', () => {
   return gulp.src('src/**/*.html')
     .pipe(ejs().on('error', gutil.log))
     .pipe(htmlmin({collapseWhitespace: true}))
     .pipe(gulp.dest('dist'))
+});
+
+gulp.task('ejs', () => {
+  return gulp.watch("src/**/*.html", ["ejs-build"]);
 });
